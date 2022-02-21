ced0bef046653b02b54f66a54421fd64c8c9afdc
aleksejunas <photo@seventyseven.no>
2022-02-21 13:35:58 +0100

    Implemented hold functionality

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -2,6 +2,14 @@
 
 /* ----- SELECTING ELEMENTS ----- */
 
+// ----- PLAYERS -----
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const name0El = document.getElementById('name--0');
+const name1El = document.getElementById('name--1');
+
+
+
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 
@@ -19,21 +27,39 @@ const btnHold = document.querySelector('.btn--hold');
 
 /* ----- STARTING CONDITIONS */
 
+const scores = [0, 0];
+
 // JavaScript automaticly converts the numbers to strings to display them on the page
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden'); // Hides the dice
 
 let currentScore = 0;
+let activePlayer = 0; // Player 1 = 0 | Player 2 = 1
 
 // ----- END OF STARTING CONDITIONS -----
 
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    /* Ternary operator checks if the active player is 0(zero), then we want the new
+     active player to be ? 1 and else : it should be 0(zero)
+     And if the active player is 1 it swithces to zero */
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    /* Toogle will add the class if it's not there and remove it if it's there
+    By toggling both at the same time, ensures that it's only on of of the elements */
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+
+}
+
+
 /* ----- ROLLING DICE FUNCTIONALITY */
 
-btnRoll.addEventListener('click', function() {
+btnRoll.addEventListener('click', function () {
     // 1. Generating a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
-    
+
     // 2. Display dice
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
@@ -43,15 +69,24 @@ btnRoll.addEventListener('click', function() {
         // Add dice to the current score
         // Option 1: currentScore = currentScore + dice;
         currentScore += dice; // Option 2
-        current0El.textContent = currentScore; // CHANGE LATER!!!
+        // Setting the score dynamicly from which is the active player right now
+        document.getElementById(`score--${activePlayer}`).textContent = currentScore;
     } else {
         // Switch to next player
-        
+        switchPlayer();
     }
 })
-
-
 // ----- ROLLING DICE FUNCTIONALITY -----
 
+btnHold.addEventListener('click', function () {
+    // 1. Add current score to the avtive player's score
+    scores[activePlayer] += currentScore;
+    // scores[1] = scores[1] + currentScore; 
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
+    // 2. Check if player's score is <= 100
+    //Finnish the game 
 
+    // Switch to the next player
+    switchPlayer();
+})
